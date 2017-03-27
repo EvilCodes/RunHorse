59de287fada3fa4d7d1469f8fbcfee68860e9e2b
blackfox <huangy_tao@126.com>
2017-03-27 21:35:17 +0800

    创建包含View和Presenter接口的契约接口

---
+++ app/src/main/java/com/yujie/hero/tasks/TaskContract.java
@@ -0,0 +1,19 @@
+package com.yujie.hero.tasks;
+
+import com.yujie.hero.BasePresenter;
+import com.yujie.hero.BaseView;
+
+/**
+ * Created by Administrator on 2017/3/27 0027.
+ */
+
+public interface TaskContract {
+    interface View extends BaseView<Presenter> {
+
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
