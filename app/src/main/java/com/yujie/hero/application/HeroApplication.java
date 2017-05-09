fc36d3e84a9eee1cca50e4b370ca7132927adc26
blackfox <huangy_tao@126.com>
2017-05-09 14:06:05 +0800

    项目整体架构的再优化

---
+++ app/src/main/java/com/yujie/hero/application/HeroApplication.java
@@ -0,0 +1,71 @@
+package com.yujie.hero.application;
+
+import android.app.Application;
+
+import com.yujie.hero.data.bean.UserBean;
+
+
+/**
+ */
+public class HeroApplication extends Application {
+
+//    public static final String SERVER_ROOT = "http://192. 168.199.127:8080/Hero/Server?";
+//    public static final String AVATAR_ROOT = "http://192.168.199.127:8080/Hero/avatar/";
+    public static final String SERVER_ROOT = "http://192.168.60.94:8080/Hero/Server?";
+    public static final String AVATAR_ROOT = "http://192.168.60.94:8080/Hero/avatar/";
+//    public static final String SERVER_ROOT = "http://127.0.0.1:8080/Hero/Server?";
+//    public static final String AVATAR_ROOT = "http://127.0.0.1:8080/Hero/avatar/";
+
+//    public static final String SERVER_ROOT = "http://10.0.2.2:8080/Hero/Server?";
+//    public static final String AVATAR_ROOT = "http://10.0.2.2:8080/Hero/avatar/";
+    public static final String EXERCISE_CODE = "10010";
+    public static final String EXAM_CODE = "10011";
+
+    public int getCURRENT_EXAM_ID() {
+        return CURRENT_EXAM_ID;
+    }
+
+    public void setCURRENT_EXAM_ID(int CURRENT_EXAM_ID) {
+        this.CURRENT_EXAM_ID = CURRENT_EXAM_ID;
+    }
+
+    private int CURRENT_EXAM_ID = 0;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public UserBean getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(UserBean currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    /**
+     * current user who signed in
+     */
+    private UserBean currentUser;
+
+    public String getCurrentTestCourse() {
+        return currentTestCourse;
+    }
+
+    public void setCurrentTestCourse(String currentTestCourse) {
+        this.currentTestCourse = currentTestCourse;
+    }
+
+    private String currentTestCourse;
+    private static HeroApplication instance = null;
+     private HeroApplication(){}
+     public static HeroApplication getInstance() {
+        synchronized (HeroApplication.class) {
+            if (instance == null) {
+                instance = new HeroApplication();
+            }
+        }
+     
+        return instance;}
+
+}
